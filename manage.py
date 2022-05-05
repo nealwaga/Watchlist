486100e2913f56942bb1218e0b000894d5e9a55e
nealwaga <waganeal@gmail.com>
2022-05-05 20:27:16 +0300

    importing flask-migrate & initializing a class

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Role
+from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 #@manage.shell decorator to create a shell context and the make_shell_context function allows us to pass in some properties into our shell. We return the app application instance, db database instance and the User class.
 @manager.shell
