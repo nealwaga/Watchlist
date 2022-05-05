2e17d922940d60d983a77241fc0d78b259214c5d
nealwaga <waganeal@gmail.com>
2022-05-05 20:58:10 +0300

    importing hashing functions & using them in User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Movie:
     '''
@@ -52,6 +53,17 @@ class User(db.Model): #Passed in db.Model as an argument that connects our class
     username = db.Column(db.String(255)) #db.String class specifies the data in that column should be a string with a maximum of 255 characters
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
     def __repr__(self): #__repr__method is not really important. It makes it easier to debug our applications
         return f'User {self.username}'
