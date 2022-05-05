2ed2b84478def0f1fbff973dee54ad0c424c61b6
nealwaga <waganeal@gmail.com>
2022-05-05 15:07:27 +0300

    imported the Role class and passed it into shell context

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User,Role
 
 #Creating app instance
 app = create_app('development')
@@ -11,7 +11,7 @@ manager.add_command('server',Server)
 #@manage.shell decorator to create a shell context and the make_shell_context function allows us to pass in some properties into our shell. We return the app application instance, db database instance and the User class.
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Role = Role)
 
 if __name__ == '__main__':
     manager.run()
