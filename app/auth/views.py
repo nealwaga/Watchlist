fdbfc0ad7294c126d4a95f1441f03c762523d822
nealwaga <waganeal@gmail.com>
2022-05-06 00:21:12 +0300

    logging out the user from the app

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
@@ -32,3 +32,9 @@ def register():
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
 
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
