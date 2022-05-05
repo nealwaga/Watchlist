5cf6c36eb019c2c2af11014cefa325f084bdfab9
nealwaga <waganeal@gmail.com>
2022-05-05 21:11:49 +0300

    registering blueprint instance

--- app/__init__.py
+++ app/__init__.py
@@ -10,20 +10,22 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    # Creating the app configurations
+  #Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    #Initializing flask extensions
+  #Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
 
-  # Registering the blueprint
+  #Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
- 
-     # setting config
+  #Setting config
     from .requests import configure_request
     configure_request(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
