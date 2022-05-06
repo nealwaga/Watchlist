4c939faf01f77b2bf3bb10537bacab97c0b3d30d
nealwaga <waganeal@gmail.com>
2022-05-06 16:25:57 +0300

    setup the SMTP server and configure the port to use the gmail SMTP server port

--- config.py
+++ config.py
@@ -7,10 +7,18 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-#Location of the database with authentication   
+    #Location of the database with authentication   
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://neal:Wneal9.@localhost/watchlist'
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
     
 class ProdConfig(Config):
