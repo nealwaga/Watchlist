9a78a3d1de3325336f4bb0159edcf9a5f28889ac
nealwaga <waganeal@gmail.com>
2022-05-06 16:27:44 +0300

    add the export code

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 export MOVIE_API_KEY='df8accb1b7d390dfcf3683c9df8d6413'
 export SECRET_KEY='-+v,.q5yv&M9!&8MuKd8?7XmgDTm5J'
 
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
+
 python3 manage.py server
