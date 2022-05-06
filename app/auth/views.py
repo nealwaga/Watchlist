6d7b8bcb895a143aba8e7c92fbce0fd4f2360480
nealwaga <waganeal@gmail.com>
2022-05-06 17:02:03 +0300

    sending welcome email

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,8 @@ from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from . import auth
+from ..email import mail_message
+
 
 
 @auth.route('/login', methods=['GET','POST'])
@@ -31,6 +33,22 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
 
 
 @auth.route('/logout')
