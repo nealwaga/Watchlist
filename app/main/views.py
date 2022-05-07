48af01f6005f0f869a4c35093d56b9b4d3cb1f72
nealwaga <waganeal@gmail.com>
2022-05-07 12:22:31 +0300

    created simgle_review function

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,7 @@ from .forms import ReviewForm, UpdateProfile
 from .. import db,photos
 from ..models import Review, User
 from flask_login import login_required, current_user
+import markdown2 #Converting markdown to HTML
 
 
 # Views
@@ -120,3 +121,11 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/review/<int:id>')
+def single_review(id):
+    review=Review.query.get(id)
+    if review is None:
+        abort(404)
+    format_review = markdown2.markdown(review.movie_review,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('review.html',review = review,format_review=format_review)
