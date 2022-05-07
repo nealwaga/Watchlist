4a80a034b08622f616ec82dac9c482e93142778a
nealwaga <waganeal@gmail.com>
2022-05-07 12:32:39 +0300

    removed validator

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import DataRequired
 
 class ReviewForm(FlaskForm):
-
     title = StringField('Review title',validators=[DataRequired()])
-    review = TextAreaField('Movie review', validators=[DataRequired()])
+    #review = TextAreaField('Movie review', validators=[DataRequired()])
+    review = TextAreaField('Movie review')
     submit = SubmitField('Submit')
 
 
